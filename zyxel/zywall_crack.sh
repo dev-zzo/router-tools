31c3e5ff2e7f87d5ed1cbb8c751b95b1174f2769
dev-zzo <dev-zzo@users.noreply.github.com>
2015-11-28 12:16:44 +0100

    Create zywall_crack.sh
    Automatic cracking of ZyWALL updates... to avoid forgetting.

---
+++ zyxel/zywall_crack.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Automatize cracking of ZyWALL update passwords
+# Uses InfoZip 2.32
+
+ZIP232=~/zip232/zip
+PKCRACK=~/pkcrack-1.2.2/src/pkcrack
+PKEXTRACT=~/pkcrack-1.2.2/src/extract
+
+FWNAME=$1
+
+if [ "x$FWNAME" = "x" ]; then
+	echo "Please provide the name of the update"
+	exit 1
+fi
+echo "Provided update name: $FWNAME"
+
+TEMP_ZIP=$FWNAME_temp.zip
+
+echo "Generating plaintext"
+$ZIP232 -9 $TEMP_ZIP $FWNAME.conf
+$PKEXTRACT $TEMP_ZIP $FWNAME.conf $FWNAME.plaintext
+rm $TEMP_ZIP
+
+echo "Starting pkcrack"
+$PKCRACK -C $FWNAME.bin -c db/etc/zyxel/ftp/conf/system-default.conf -p $FWNAME.plaintext
+
+echo "Done!"
